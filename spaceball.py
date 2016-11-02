24d9f9ff77d533e19ffae7a0af2bb30655f833f9
Pete Richardson <pete@peterichardson.com>
2016-11-02 08:13:47 -0700

    Added ability to set button and data handlers

--- spaceball.py
+++ spaceball.py
@@ -1,10 +1,19 @@
 import serial
 import re
 
-class SpaceBallDataEvent:
+class SpaceBallEvent:
     def __init__(self, type, data):
         self.type = type
         self.data = data
+
+    @classmethod
+    def create(cls, type, data):
+        if type == 'K':
+            return SpaceBallButtonEvent(data)
+        elif type == 'D':
+            return SpaceBallDataEvent(data)
+        else:
+            return SpaceBallEvent(type, data)
 
     def __str__(self):
         result = ""
@@ -14,10 +23,58 @@ class SpaceBallDataEvent:
             result += "{:02x}".format(ord(self.type))
             #brep = "{:08b}".format(ord(c))
         for c in self.data:
-            result += " {:02x}".format(ord(c))
+            result += " {:02x}".format(c)
         return result
 
+    def run(self, handlers):
+        pass
+
+class SpaceBallButtonEvent (SpaceBallEvent):
+    def __init__(self, data):
+        SpaceBallEvent.__init__(self, 'K', data)
+
+    def __str__(self):
+        print "Key Event! ",
+        return SpaceBallEvent.__str__(self)
+
+    def run(self, handlers):
+        if self.data[0] & 0x1F:    # check pick button and buttons 5-8
+            if (self.data[0] & 0x10):
+                handlers["pick_button_up"](self)
+            if (self.data[0] & 0x01):
+                handlers["5_button_up"](self)
+            if (self.data[0] & 0x02):
+                handlers["6_button_up"](self)
+            if (self.data[0] & 0x04):
+                handlers["7_button_up"](self)
+            if (self.data[0] & 0x08):
+                handlers["8_button_up"](self)
+
+        if self.data[1] & 0x0F:    # check buttons 1-4
+            if (self.data[1] & 0x01):
+                handlers["1_button_up"](self)
+            if (self.data[1] & 0x02):
+                handlers["2_button_up"](self)
+            if (self.data[0] & 0x04):
+                handlers["3_button_up"](self)
+            if (self.data[0] & 0x08):
+                handlers["4_button_up"](self)
+
+
+class SpaceBallDataEvent (SpaceBallEvent):
+    def __init__(self, data):
+        SpaceBallEvent.__init__(self, 'D', data)
+
+    def __str__(self):
+        print "Data Event! ",
+        return SpaceBallEvent.__str__(self)
+
+    def run(self, handlers):
+        handlers["data"](self)
+
+
 class SpaceBall:
+
     def __init__(self, tty='/dev/tty.USA19QW1P1.1'):
         self.tty = tty
         self.ser = serial.Serial(timeout=None)
@@ -33,25 +90,42 @@ class SpaceBall:
             "P@r@r\x0D",            # Data rate to 20 events per Second
             "MSSV\x0D",             # Ball Event Type: Trans and Rot Vectors
             "Z\x0D",                # Rezero the ball
-            "BcCc\x0D",             # Beep it
+            "Bc\x0D",               # Beep it
             "H"
         ]
         for msg in config_data:
             self.ser.write(msg)
 
-    def read(self):
-        return self.ser.read()
+        self.handlers = dict.fromkeys([
+            'pick_button_up',
+            '1_button_up',
+            '2_button_up',
+            '3_button_up',
+            '4_button_up',
+            '5_button_up',
+            '6_button_up',
+            '7_button_up',
+            '8_button_up',
+            'data'
+            ], lambda e: None
+        )
 
     def read_event(self):
-        type = self.read()
+        type = self.ser.read()
         data = []
-        c = self.read()
+        c = self.ser.read()
         while c != '\x0D':
-            data.append(c)
-            c = self.read()
-        result = SpaceBallDataEvent(type, data)
+            data.append(ord(c))
+            c = self.ser.read()
+        result = SpaceBallEvent.create(type, data)
         return result
 
+    def update(self):
+        e = self.read_event()
+        e.run(self.handlers)
+
+    def register_handler(self, regex, handler):
+        pass
 
 
 
--- test_spaceball.py
+++ test_spaceball.py
@@ -1,6 +1,15 @@
 from spaceball import SpaceBall
 
 ball = SpaceBall()
+
+def handle_1_button(event):
+	print ("1 button pushed!")
+
+def dump_data(event):
+	print event
+
+ball.handlers['data'] = dump_data
+ball.handlers['1_button_up'] = handle_1_button
+
 while True:
-    e = ball.read_event()
-    print e
+    ball.update()
